7efd6ff56a1aa9b9217abb65715d3f0a755e244e
Adyaprana <120256356+Adyaprana@users.noreply.github.com>
2026-06-03 07:28:52 +0530

    adding the 02.py file

---
+++ DAY-2/02.py
@@ -0,0 +1,12 @@
+# This is the second file of the second day of the Backend Developer Journey. In this file, we will learn about type conversion in Python and how to convert one data type to another.
+# Type conversion is the process of converting a value from one data type to another. In Python
+age = input("Enter your age:")
+print("the type of the age is:",type(age))
+
+# Everything from input() comes as STRING.
+# Convert Input to Integer
+
+age = int(input("Enter your age:"))
+print("the type of the age is:",type(age))
+
+
