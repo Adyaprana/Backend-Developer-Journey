d36c390c75cf6c0c3522c56e7ec1072bca40f5b6
Adyaprana <120256356+Adyaprana@users.noreply.github.com>
2026-06-03 07:26:20 +0530

    adding 01.py

---
+++ DAY-2/01.py
@@ -0,0 +1,5 @@
+# This is the first file of the second day of the Backend Developer Journey. In this file, we will learn how to take input from the user and also learn about string formatting.
+name = input("Enter your name: ")
+age = input("Enter your age:")
+print("Welcome",name)
+print(f"You are {age} years old")
