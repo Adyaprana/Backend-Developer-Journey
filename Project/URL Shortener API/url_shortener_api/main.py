ebe656048ece2044a736f75b5b0f3771037cade5
Adyaprana <120256356+Adyaprana@users.noreply.github.com>
2026-08-13 19:59:35 +0530

    url_shortener_api (phase4 Day - 7 & 8)

--- Project/URL Shortener API/url_shortener_api/main.py
+++ Project/URL Shortener API/url_shortener_api/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from app.database.base import Base
+from app.database.database import engine
+from app.models.shortened_url import ShortenedURL
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="URL Shortener API",
     version="1.0.0",
     description="A URL Shortener API built with FastAPI and PostgreSQL."
 )
-
 
 @app.get("/")
 def root():
@@ -15,4 +19,10 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
