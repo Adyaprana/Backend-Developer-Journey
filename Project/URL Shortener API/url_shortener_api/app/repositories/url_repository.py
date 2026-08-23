f7d5a4c8c21ae9e47d079f14cb411b30d75ef4d5
Adyaprana <120256356+Adyaprana@users.noreply.github.com>
2026-08-23 15:22:34 +0530

    url_shortener_api (phase7 Day - 14, 15 & 16)

--- Project/URL Shortener API/url_shortener_api/app/repositories/url_repository.py
+++ Project/URL Shortener API/url_shortener_api/app/repositories/url_repository.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models.shortened_url import ShortenedURL
+from typing import Optional
 class URLRepository:
     """
     Handles all database operations for shortened URLs.
@@ -12,3 +13,17 @@ class URLRepository:
         db.commit()
         db.refresh(url)
         return url
+
+    def get_by_short_code(
+        self,
+        db: Session,
+        short_code: str
+    ) -> Optional[ShortenedURL]:
+        """
+        Return a URL by its short code.
+        """
+        return (
+            db.query(ShortenedURL)
+            .filter(ShortenedURL.short_code == short_code)
+            .first()
+        )
